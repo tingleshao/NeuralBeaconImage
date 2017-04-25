1ca166afdf4951a1ed67d9c11cecee33e440719d
marbleimplementer <marbleimplementer@gmail.com>
2017-04-25 15:18:57 -0400

    first commit

---
+++ app/src/main/java/com/example/chongshao/neuralbeaconimage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chongshao.neuralbeaconimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
